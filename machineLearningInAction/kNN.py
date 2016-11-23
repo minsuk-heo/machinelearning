d106b443b1f7093001b99314018ba30a3ebf5240
minsuk-heo <minsuk.heo@gmail.com>
2016-11-23 07:50:26 -0800

    kNN.py updated

--- machineLearningInAction/kNN.py
+++ machineLearningInAction/kNN.py
@@ -6,3 +6,23 @@ def createDataSet():
     labels = ['A', 'B', 'B', 'B']
     return group, labels
 
+def classify0(inX, dataSet, labels, k):
+    dataSetSize = dataSet.shape[0]
+    diffMat = tile(inX, (dataSetSize,1)) - dataSet
+    sqDiffMat = diffMat ** 2
+    sqDistances = sqDiffMat.sum(axis = 1)
+    distances = sqDistances ** 0.5
+    sortedDistIndices = distances.argsort()
+    classCount = {}
+
+    for i in range(k):
+        voteIlabel = labels[sortedDistIndices[i]]
+        classCount[voteIlabel] = classCount.get(voteIlabel, 0) + 1
+
+    sortedClassCount = sorted(classCount.iteritems(), key=operator.itemgetter(1), reverse=True)
+    return sortedClassCount[0][0]
+
+
+group, labels = createDataSet()
+result = classify0([0,0], group, labels,3)
+print result
