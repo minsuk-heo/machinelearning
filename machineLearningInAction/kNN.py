653d339bb5068aa9f7c2ad1cafca28e98f81e357
minsuk-heo <minsuk.heo@gmail.com>
2016-11-24 06:17:59 -0800

    kNN.py updated

--- machineLearningInAction/kNN.py
+++ machineLearningInAction/kNN.py
@@ -2,8 +2,8 @@ from numpy import *
 import operator
 
 def createDataSet():
-    group = array([ [1.0, 1.1], [1.0,1.0], [0, 0], [0, 0.1] ])
-    labels = ['A', 'B', 'B', 'B']
+    group = array([ [1.0, 2.0], [1.0,4.0], [4.0, 1.0], [4.0, 2.0] ])
+    labels = ['Action', 'Action', 'Romantic', 'Romantic']
     return group, labels
 
 def calcDistance(inX, dataSet, labels, k):
@@ -14,19 +14,20 @@ def calcDistance(inX, dataSet, labels, k):
     # numpy.tile(A, reps): construct an array by repeating A the number of times given by reps
     # if reps has length d, the result will have dimension of max(d, A.ndim)
     # tile(inX, (dataSetSize,1)) will return [ [0,0] [0,0] [0,0] [0,0] ]
-    # diffMat is [ [-1, -1.1], [-1, -1], [0, 0], [0, -0.1] ]
+    # diffMat is [ [1, 1], [1, -1], [-2, 2], [-2, 1] ]
     diffMat = tile(inX, (dataSetSize, 1)) - dataSet
+
     # **2 means square
     sqDiffMat = diffMat ** 2
 
     # sqDistances = x^2 + y^2
     sqDistances = sqDiffMat.sum(axis=1)
     # distance is equal to the square root of the sum of the squares of the coordinates
-    # distance = [1.48, 1.41, 0, 0.1]
+    # distance = [2, 2, 8, 5]
     distances = sqDistances ** 0.5
 
     # numpy.argsort() returns the indices that would sort an array
-    # here returns [2 3 1 0]
+    # here returns [0 1 3 2]
     sortedDistIndices = distances.argsort()
     return sortedDistIndices
 
@@ -38,7 +39,10 @@ def findMajorityClass(inX, dataSet, labels, k, sortedDistIndices):
         voteIlabel = labels[sortedDistIndices[i]]
         # increase +1 on the selected label
         classCount[voteIlabel] = classCount.get(voteIlabel, 0) + 1
+
+    # classCount dictionary : {'Action': 2, 'Romantic': 1}
     # sort ClassCount Descending order
+
     return sorted(classCount.iteritems(), key=operator.itemgetter(1), reverse=True)
 
 
@@ -47,10 +51,10 @@ def classify0(inX, dataSet, labels, k):
     sortedDistIndices = calcDistance(inX, dataSet, labels, k)
     # take k items with lowest distances to inX and find the majority class among k items
     sortedClassCount = findMajorityClass(inX, dataSet, labels, k, sortedDistIndices)
-    # sortedClassCount is now [('B', 3)], therefore return B
+    # sortedClassCount is now [('Action', 2)], therefore return Action
     return sortedClassCount[0][0]
 
 
 group, labels = createDataSet()
-result = classify0([0,0], group, labels,3)
+result = classify0([2.0, 3.0], group, labels,3)
 print result
